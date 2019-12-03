9143519d0f1e409626d87cd22c3981abe984e98a
oldmon <oldmonou@gmail.com>
2019-12-03 15:51:21 +0800

    add logging facilities

--- smtpmail.py
+++ smtpmail.py
@@ -6,12 +6,15 @@ import csv
 import sys
 import time
 import smtplib
+import logging
 from email.mime.text import MIMEText
 from email.header import Header
-
+counter = 1
 sender = 'service@sync.cloudbox.hinet.net'
 subject = 'hicloud box(e)功能發佈'
 fromname = 'hicloud box(e)'
+
+logging.basicConfig(filename='logger.log', level=logging.INFO)
 
 if len(sys.argv) != 3:
     print("Usage: python3 "+sys.argv[0]+" MAILLIST MAILCONTENT")
@@ -26,9 +29,8 @@ with open(MAILLIST, 'r', encoding='utf8')as csvfile:
     rows = csv.reader(csvfile)
     for row in rows:
         name = row[1]
-        receivers = row[0]
-
-        message = MIMEText(MESG, 'plain', 'utf-8')
+        receiver = row[0]
+        message = MIMEText(MESG, 'html', 'utf-8')
         message['From'] = Header(fromname, 'utf-8')
         message['To'] = Header(name, 'utf-8')
         message['Subject'] = Header(subject, 'utf-8')
@@ -36,9 +38,11 @@ with open(MAILLIST, 'r', encoding='utf8')as csvfile:
         try:
             smtpObj = smtplib.SMTP('www.hibox.hinet.net')
             smtpObj.login('service@sync.cloudbox.hinet.net', 'coidc805')
-            smtpObj.sendmail(sender, receivers, message.as_string())
-            print("郵件發送成功")
+            smtpObj.sendmail(sender, receiver, message.as_string())
+            #print("郵件發送成功")
+            logging.info("Mail sent to %d: %s"% (counter, receiver))
         except smtplib.SMTPException:
-            print("Error: 無法發送郵件")
-
+            #print("Error: 無法發送郵件")
+            logging.error("Error: cannot sent to %d: %s"% (counter,reveiver))
+        counter += 1
         time.sleep(8)
